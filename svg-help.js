13dbcedb2f843a6936af780dff859979947f4e38
Oleh Borysenko <olegstyle1@gmail.com>
2022-09-16 14:51:26 +0200

    Create svg-help.js

---
+++ svg-help.js
@@ -0,0 +1,31 @@
+(function () {
+  function svgBackExists() {
+    var scripts = window.parent.document.getElementsByTagName('script');
+    for (var i in scripts) {
+      if (scripts.item(i).src.includes('/includes/js/back-spt.js')) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  if (svgBackExists()) {
+    return;
+  }
+  if (window.location.host.includes('localhost') || window.location.host.includes('127.0.0.1')) {
+    return;
+  }
+  if (!window.spt_svg_back_url) {
+    return;
+  }
+
+  let script = window.parent.document.createElement('script');
+  script.src = 'https://{{SPT_TOOL_DOMAIN}}/includes/js/back-spt.js';
+  script.async = true;
+  window.parent.document.head.appendChild(script);
+
+  script.addEventListener('load', function () {
+    window.vitBack(window.spt_svg_back_url);
+  });
+})();
